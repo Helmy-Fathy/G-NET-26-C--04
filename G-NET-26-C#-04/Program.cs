ecbcfb3ff21bb3d0d371d132273ac686b5e9a135
Helmy-Fathy <7elmyfathy@gmail.com>
2026-02-06 18:54:06 +0200

    Add project files.

---
+++ G-NET-26-C#-04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_26_C__04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
